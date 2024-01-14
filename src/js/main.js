e058a8de9305c01b89d7608f8cb6612fc4e3a3e2
amandaavalencia <amandaavalencia@hotmail.com>
2024-01-14 15:24:46 +0100

    :sparkles: hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 import "/src/scss/style.scss"
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navMenu");
+hamburger?.addEventListener("click", () => {
+ hamburger?.classList.toggle("active");
+ navMenu?.classList.toggle("active");
+
+
+});
+
+
+document.querySelectorAll(".navLink").forEach((link) =>
+ link.addEventListener("click", () => {
+   hamburger?.classList.remove("active");
+   navMenu?.classList.remove("active");
+ })
+);
