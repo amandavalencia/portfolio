afbaf9c45d44a3091c953c9075476271dafece19
amandaavalencia <amandaavalencia@hotmail.com>
2024-01-29 09:20:11 +0100

    @media for desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 import "/src/scss/style.scss"
 
-
+//------Hamburger menu-------
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
 hamburger?.addEventListener("click", () => {
  hamburger?.classList.toggle("active");
  navMenu?.classList.toggle("active");
-
 
 });
 
